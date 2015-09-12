55522eb8aca4e018f9a81e3f0dd9ab5619f53904
YevgeniySt <strogonov-jeks@mail.ru>
2015-09-12 18:41:05 +0300

    V2

---
+++ YS_SeleniumWebDriverTests_Basic/src/test/java/YS_SeleniumTests/YS_SeleniumCreatePlayerTest.java
@@ -0,0 +1,12 @@
+package YS_SeleniumTests;
+
+/**
+ * Created by Евгений on 12.09.2015.
+ */
+public class YS_SeleniumCreatePlayerTest {
+
+    public static void main(String[] args){
+
+        Web
+    }
+}
