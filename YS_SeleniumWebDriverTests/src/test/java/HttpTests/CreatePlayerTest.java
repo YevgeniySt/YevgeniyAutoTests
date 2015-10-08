9bbaaac70af0568ed5f668077690d3b1ddda62e9
YevgeniySt <strogonov-jeks@mail.ru>
2015-10-08 19:53:38 +0300

    V18

--- YS_SeleniumWebDriverTests/src/test/java/HttpTests/CreatePlayerTest.java
+++ YS_SeleniumWebDriverTests/src/test/java/HttpTests/CreatePlayerTest.java
@@ -3,23 +3,18 @@ package HttpTests;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -35,33 +30,34 @@ public class CreatePlayerTest {
     @Test
     public void createPlayerTest() throws IOException, InterruptedException {
 
-        HttpClient httpclient = HttpClients.createDefault();
-        HttpPost post = new HttpPost(baseUrl+"/auth/login");
+        //create http client
 
-// post
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        //create post
+
+        HttpPost post = new HttpPost(baseUrl+"/auth/login");
         List<NameValuePair> params = new ArrayList<NameValuePair>(2);
         params.add(new BasicNameValuePair("username", "admin"));
         params.add(new BasicNameValuePair("password", "123"));
         params.add(new BasicNameValuePair("logIn", "Log+In"));
-
         post.setEntity(new UrlEncodedFormEntity(params));
 
-        HttpResponse response = httpclient.execute(post);
+        //execute post
+        CloseableHttpResponse response = httpclient.execute(post);
 
-
+        //get status
         System.out.println(response.getStatusLine().getStatusCode());
 
-    //get
+        //create get
 
-        String url = baseUrl+"/players";
+        HttpGet get = new HttpGet(baseUrl+"/players");
 
+        //execute get
 
-        HttpGet get = new HttpGet(url);
+        CloseableHttpResponse response1 = httpclient.execute(get);
 
-        // add request header
-
-
-        HttpResponse response1 = httpclient.execute(get);
+        //get response
 
         BufferedReader rd1 = new BufferedReader(
                 new InputStreamReader(response1.getEntity().getContent()));
@@ -71,24 +67,24 @@ public class CreatePlayerTest {
         while ((line1 = rd1.readLine()) != null) {
             result1.append(line1);
         }
-       // System.out.println(result1.toString());
+
+        //check title
+
         boolean b =result1.toString().contains("<title>Players</title>");
         System.out.println(b);
 
+        //close response
 
-        //post
-        params.clear();
+        response1.close();
+
+        //create post
+
         String name = "UN" + String.valueOf(new SimpleDateFormat("MMddHHmmss").format(new Date()));
         String email = "UN" + String.valueOf(new SimpleDateFormat("MMddHHmmss").format(new Date()))+"@mail.ru";
-        HttpClient httpclient1 = HttpClients.createDefault();
         HttpPost post2 = new HttpPost(baseUrl+"/players/insert");
 
-        //
-        //HttpPost uploadFile = new HttpPost("...");
-
         MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-       // builder.seContentType(ContentType.parse("multipart/form-data"));
-        //builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+
         builder.addTextBody("#requestSender", "ff14642ac1c");
         builder.addTextBody("submitMode", "save");
         System.out.println(name);
@@ -126,80 +122,11 @@ public class CreatePlayerTest {
         builder.addTextBody("ff14642ac1c__stop_weekly_reset", "0");
 
         HttpEntity multipart = builder.build();
-
         post2.setEntity(multipart);
+        CloseableHttpResponse response5 = httpclient.execute(post2);
 
-        HttpResponse response5 = httpclient1.execute(post2);
+        //check status
         System.out.println(response5.getStatusLine().getStatusCode());
-        //
-
-
-
-
-
-
-
-       // params.add(new BasicNameValuePair("#requestSender", "ff14642ac1c"));
-/*
-
-        params.add(new BasicNameValuePair("submitMode", "save"));
-
-        String name = "UN" + String.valueOf(new SimpleDateFormat("MMddHHmmss").format(new Date()));
-        String email = "UN" + String.valueOf(new SimpleDateFormat("MMddHHmmss").format(new Date()))+"@mail.ru";
-        System.out.println(name);
-        System.out.println(email);
-        params.add(new BasicNameValuePair("ff14642ac1c__us_login", name));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_email", email));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_password", "Password1"));
-        params.add(new BasicNameValuePair("ff14642ac1c__confirm_password", "Password1"));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_reg_status", "1"));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_chat_enabled", "0"));
-       params.add(new BasicNameValuePair("ff14642ac1c__us_chat_enabled", "1"));
-        params.add(new BasicNameValuePair("ff14642ac1c__is_community", "0"));
-        params.add(new BasicNameValuePair("ff14642ac1c__is_community", "1"));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_owner_id", "1"));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_fname", ""));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_lname", ""));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_city", ""));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_country", "_unset_"));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_address", ""));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_phone", "123456"));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_gender", "1"));
-        params.add(new BasicNameValuePair("ff14642ac1c__us_birthday", "10-07-1997"));
-        params.add(new BasicNameValuePair("ff14642ac1c__disable_lp", "0"));
-        params.add(new BasicNameValuePair("ff14642ac1c__payment_chat_enabled", "0"));
-        params.add(new BasicNameValuePair("ff14642ac1c__payment_chat_enabled", "1"));
-        params.add(new BasicNameValuePair("ff14642ac1c__pit_boss_alert_enabled", "0"));
-        params.add(new BasicNameValuePair("ff14642ac1c__pit_boss_alert_enabled", "1"));
-        params.add(new BasicNameValuePair("ff14642ac1c__credit_type", "Periodic Credit"));
-        params.add(new BasicNameValuePair("ff14642ac1c__credit_amount", ""));
-        params.add(new BasicNameValuePair("ff14642ac1c__credit_frequency", "Weekly"));
-        params.add(new BasicNameValuePair("ff14642ac1c__credit_next_reset", "10-12-2015 00:00:00"));
-        params.add(new BasicNameValuePair("ff14642ac1c__credit_temp", ""));
-        params.add(new BasicNameValuePair("ff14642ac1c__credit_expiration", ""));
-        params.add(new BasicNameValuePair("ff14642ac1c__credit_comment", ""));
-        params.add(new BasicNameValuePair("ff14642ac1c__stop_weekly_reset", "0"));
-
-        post2.setEntity(new UrlEncodedFormEntity(params));
-
-        HttpResponse response2 = httpclient1.execute(post2);
-
-
-        System.out.println(response2.getStatusLine().getStatusCode());
-        */
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
