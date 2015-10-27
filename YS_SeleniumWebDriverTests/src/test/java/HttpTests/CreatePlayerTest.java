cfc3d0a4c772cd6dfd0353d61b0d778ae13fb4c5
YevgeniySt <strogonov-jeks@mail.ru>
2015-10-27 22:14:09 +0200

    V25

--- YS_SeleniumWebDriverTests/src/test/java/HttpTests/CreatePlayerTest.java
+++ YS_SeleniumWebDriverTests/src/test/java/HttpTests/CreatePlayerTest.java
@@ -26,13 +26,7 @@ import java.util.Properties;
  */
 public class CreatePlayerTest extends BaseHttp{
 
-    // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/test";
 
-    //  Database credentials
-    static final String USER = "root";
-    static final String PASS = "";
 
 
     @Test
@@ -81,71 +75,13 @@ public class CreatePlayerTest extends BaseHttp{
 
         HttpUtils.httpPostSearchPlayer(baseUrl + "/players", expectedPlayer);
         //assert
-        String us_login = null;
-        Connection conn = null;
-        Statement stmt = null;
-        try{
-            //STEP 2: Register JDBC driver
-            Class.forName("com.mysql.jdbc.Driver");
-
-            //STEP 3: Open a connection
-            System.out.println("Connecting to a selected database...");
-            conn = (Connection) DriverManager.getConnection(DB_URL, USER, PASS);
-            System.out.println("Connected database successfully...");
-
-            //STEP 4: Execute a query
-            System.out.println("Creating statement...");
-            stmt = (Statement) conn.createStatement();
-
-            String sql ="SELECT us_login FROM players where us_login="+"\""+expectedPlayer.login+"\"";
-            //System.out.print(sql);
-            ResultSet rs = stmt.executeQuery(sql);
-
-
-            //STEP 5: Extract data from result set
-            while(rs.next()){
-                //Retrieve by column name
-
-                us_login = rs.getString("us_login");
-
-
-                //Display values
-                System.out.println("us_login: " + us_login);
-            }
-            rs.close();
-        }catch(SQLException se){
-            //Handle errors for JDBC
-            se.printStackTrace();
-        }catch(Exception e){
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        }finally{
-            //finally block used to close resources
-            try{
-                if(stmt!=null)
-                    conn.close();
-            }catch(SQLException se){
-            }// do nothing
-            try{
-                if(conn!=null)
-                    conn.close();
-            }catch(SQLException se){
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        System.out.println("Goodbye!");
-
-
-
-
-
-
+        String us_loginActual = HttpUtils.queryResult("SELECT us_login FROM players where us_login="+"\""+expectedPlayer.login+"\"");
 
       //  StringBuffer result6 = HttpUtils.httpGetUrl(baseUrl + "/players");
 
        // Assert.assertTrue(result6.toString().contains(">" + expectedPlayer.login + "</a>k"));
 
-        Assert.assertEquals(expectedPlayer.login+"f",us_login);
+        Assert.assertEquals(expectedPlayer.login+"f",us_loginActual);
 
 
 
--- YS_SeleniumWebDriverTests/src/test/java/Utils/HttpUtils.java
+++ YS_SeleniumWebDriverTests/src/test/java/Utils/HttpUtils.java
@@ -4,6 +4,7 @@ import Objects.PlayerFull;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
+import com.mysql.jdbc.Statement;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -20,6 +21,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -30,6 +33,13 @@ import java.util.Properties;
  * Created by Евгений on 09.10.2015.
  */
 public class HttpUtils {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/test";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "";
 
     static CloseableHttpClient httpclient = HttpClients.createDefault();
 
@@ -141,6 +151,62 @@ public class HttpUtils {
 
         CloseableHttpResponse r =  httpclient.execute(post2);
         r.close();
+    }
+    public static String queryResult(String query){
+        String us_login = null;
+        com.mysql.jdbc.Connection conn = null;
+        Statement stmt = null;
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = (com.mysql.jdbc.Connection) DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = (Statement) conn.createStatement();
+
+            String sql =query;
+            //System.out.print(sql);
+            ResultSet rs = stmt.executeQuery(sql);
+
+
+            //STEP 5: Extract data from result set
+            while(rs.next()){
+                //Retrieve by column name
+
+                us_login = rs.getString("us_login");
+
+
+                //Display values
+                System.out.println("us_login: " + us_login);
+            }
+            rs.close();
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    conn.close();
+            }catch(SQLException se){
+            }// do nothing
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+        return us_login;
     }
 
 
